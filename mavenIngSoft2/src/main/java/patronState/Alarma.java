5acf11817e7ea0e35d6e4ddbb9706ac135c76333
unknown <cms196@alumnos.unican.es>
2021-03-22 18:18:12 +0100

    Implementacion metodos negocio clase Alarmas

--- mavenIngSoft2/src/main/java/patronState/Alarma.java
+++ mavenIngSoft2/src/main/java/patronState/Alarma.java
@@ -1,12 +1,12 @@
 package patronState;
 import java.util.Date;
 
-public class Alarma {
-	
+public class Alarma implements Comparable<Alarma>{
+
 	//Atributos privados de la clase Alarma
 	private String id;
 	private Date hora;
-	
+
 	//Constructor de la clase alarma
 	public Alarma (String id, Date hora) {
 		this.id = id;
@@ -17,9 +17,27 @@ public class Alarma {
 	public String getId() {
 		return this.id;
 	}
-	
+
 	//Mñetod getter de la hora de una alarma
 	public Date getHora() {
 		return this.hora;
+	}
+
+	public int compareTo(Alarma a) {
+
+		int retorno ;
+		Date alarmaDate = a.getHora();
+		Date actualDate=java.util.Calendar.getInstance().getTime();
+
+		if (alarmaDate.compareTo(actualDate) > 0) {
+			retorno = -1;
+		} 
+		if (alarmaDate.compareTo(actualDate) < 0) { //Si es antes la alarma que la fecha actual
+			retorno = 1;
+		} else {
+			retorno = 0;
+		}
+		return retorno;
+
 	}
 }
--- mavenIngSoft2/src/main/java/patronState/Alarmas.java
+++ mavenIngSoft2/src/main/java/patronState/Alarmas.java
@@ -1,16 +1,24 @@
 package patronState;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class Alarmas {
-	
+
 	private AlarmasState state;
-	
+	//Facil conseguir la alarma siguiente y ordenarlas en función del tiempo restante
+	private PriorityQueue<Alarma> alarmasActivas = new PriorityQueue<Alarma>();
+	//Complejidad temporal de búsqueda, insercción y eliminacion O(1)
+	//Key:Id de la alarma
+	private HashMap<String, Alarma> alarmasDesactivadas = new HashMap<String,Alarma>();
+	private HashMap<String, Alarma> alarmasTotales = new HashMap<String,Alarma>();
+
 	//Constructor de la clase Alarmas
 	public Alarmas() {
 		state = AlarmasState.init(this);
 	}
-	
+
 	//Implementacion de los métodos de señal
 	public void setState(AlarmasState value) {
 		this.state = value;
@@ -30,28 +38,76 @@ public class Alarmas {
 	public void alarmaOn(String id, Alarmas context) {
 		state.alarmaOn(id, this);
 	}
-	
+
 	//Implementacion de los método de negocio (REVISAR DATO QUE DEVUELVE TODOS VOID ESQUELETO)
-	public void alarma(String id) {
-		//Código de negocio
+	/**
+	 * Método que devuelve la alarma en función del id
+	 * @param id
+	 * @return La alarma que tiene como identificador el string introducido
+	 */
+	public Alarma alarma(String id) {
+		Alarma alarmaBuscada = alarmasTotales.get(id);
+		if (alarmaBuscada == null) {
+			return null;
+		} else {
+			return alarmaBuscada;
+		}
 	}
-	public void anhadeAlarma(Alarma a) {
-		//Código de negocio
+
+	/**
+	 * Añade una nueva alarma preparada para sonar.
+	 * @param alarmaAñadir
+	 * @return true si se añade la alarma
+	 * @return false si ya había una alarma para esa hora
+	 */
+	public boolean anhadeAlarma(Alarma alarmaAnhadir) {
+		Date hora = alarmaAnhadir.getHora();
+		if(alarmasTotales.containsValue(hora)) {
+			return false;
+		} else {
+			alarmasTotales.put(alarmaAnhadir.getId(), alarmaAnhadir);
+			alarmasActivas.add(alarmaAnhadir);
+		}
+		return true;
 	}
-	public void eliminaAlarma(Alarma a) {
-		//Código de negocio
+
+	/**
+	 * 
+	 * @param alarmaEliminar alarma a eliminar 
+	 */
+	public boolean eliminaAlarma(Alarma alarmaEliminar) {
+		String idAlarma = alarmaEliminar.getId();
+
+		if (!alarmasTotales.containsKey(idAlarma)) {
+			return false;
+		} else if (alarmasDesactivadas.containsKey(idAlarma)) {
+			alarmasDesactivadas.remove(idAlarma);
+			alarmasTotales.remove(idAlarma);
+			return true;
+		} else {
+			alarmasActivas.remove(alarmaEliminar);
+			alarmasTotales.remove(idAlarma);
+			return true;
+		}
 	}
-	public void alarmaMasProxima() {
-		//Código de negocio 
+	
+	public Alarma alarmaMasProxima() {
+		return alarmasActivas.peek(); 
 	}
-	public void desactivaAlarma(Alarma a) {
-		//Código de negocio
+	
+	public void desactivaAlarma(Alarma alarmaDesactivar) {
+		alarmasActivas.remove(alarmaDesactivar);
+		alarmasDesactivadas.put(alarmaDesactivar.getId(), alarmaDesactivar);
 	}
+	
 	public void activarMelodia() {
 		//Código de negocio
+		//¿COMO HAGO QUE SUENE?¿JAVA SWING?
 	}
 	public void desactivarMelodia() {
 		//Código de negocio
+		//¿COMO HAGO QUE SUENE?¿JAVA SWING?
 	}
+	
 }
 
